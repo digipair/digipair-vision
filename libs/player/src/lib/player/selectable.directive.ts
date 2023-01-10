48e61cc5e7ba103dbf05245410623ae212692d2d
Marc Buils <marc.buils@marcbuils.fr>
2023-01-10 11:45:21 +0100

    fix: hand tracking with teleport

--- libs/player/src/lib/player/selectable.directive.ts
+++ libs/player/src/lib/player/selectable.directive.ts
@@ -9,17 +9,22 @@ AFRAME.registerComponent('selectable', {
     this.worldPosition = new THREE.Vector3();
     this.handEls = document.querySelectorAll('[hand-tracking-controls]');
     this.pressed = false;
+    this.step = 0;
   },
 
   tick: function () {
+    this.step = (this.step + 1) % 10;
+
+    if (this.step > 0) {
+      return;
+    }
+
     const handEls = this.handEls;
     let handEl: Entity;
     let distance: number;
     for (let i = 0; i < handEls.length; i++) {
       handEl = handEls[i];
-      distance = this.calculateFingerDistance(
-        (handEl.components['hand-tracking-controls'] as any).indexTipPosition
-      );
+      distance = this.calculateFingerDistance(handEl);
       if (distance < this.data.pressDistance) {
         // if (!this.pressed) { this.el.emit('pressedstarted'); }
         this.pressed = true;
@@ -32,9 +37,16 @@ AFRAME.registerComponent('selectable', {
     this.pressed = false;
   },
 
-  calculateFingerDistance: function (fingerPosition: any) {
+  calculateFingerDistance: function (handEl: any) {
     const el = this.el;
     const worldPosition = this.worldPosition;
+    const fingerPosition = new THREE.Vector3();
+
+    fingerPosition.copy(
+      (handEl.components['hand-tracking-controls'] as any).indexTipPosition
+    );
+    handEl.object3D.parent.updateMatrixWorld();
+    handEl.object3D.parent.localToWorld(fingerPosition);
 
     worldPosition.copy(el.object3D.position);
     el.object3D.parent.updateMatrixWorld();
