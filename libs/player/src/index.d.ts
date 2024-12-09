44e3dc811b4a4f1e01a489ba1456663aa1fde5f7
Raphael Rattelli <raphael.rattelli@epitech.eu>
2024-12-09 11:57:34 +0100

    fix: libs/html is working

--- libs/player/src/index.d.ts
+++ libs/player/src/index.d.ts
@@ -1,1 +1,4 @@
-export * from './src/index';
+export * from './src/lib/pins/pins.interface';
+export * from './src/lib/teleport/teleportable';
+export * from './src/lib/player/player.element';
+export * from './src/lib/player/player.provider';
