e32d5045f8384f98c847543aafd5806ba0c8b91b
Raphael Rattelli <raphael.rattelli@epitech.eu>
2024-12-12 12:04:47 +0100

    feat: ready-player-me is working

---
+++ libs/ready-player-me/rollup.build.cjs
@@ -0,0 +1,33 @@
+const tsconfig = require('../../tsconfig.base.json');
+const alias = require('@rollup/plugin-alias');
+const path = require('path');
+
+function getRollupOptions(options) {
+  const extraGlobals = {};
+  const externals = options.output.format === 'cjs' ? ['@digipair-vision/core','@digipair-vision/mesh'] : [];
+
+  const value = {
+    ...options,
+    external: name => {
+      return externals.includes(name);
+    },
+    plugins: [
+      ...options.plugins,
+      alias({
+        entries: Object.getOwnPropertyNames(tsconfig.compilerOptions.paths).map(property => ({
+          find: property,
+          replacement: path.resolve(tsconfig.compilerOptions.paths[property][0]),
+        })),
+      }),
+    ],
+    output: {
+      ...options.output,
+      globals: extraGlobals,
+      plugins: [],
+    },
+  };
+
+  return value;
+}
+
+module.exports = getRollupOptions;
