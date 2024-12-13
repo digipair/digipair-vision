76a5fc797af649f423c7968dd887fd5a2a5b1432
Raphael Rattelli <raphael.rattelli@epitech.eu>
2024-12-13 10:03:58 +0100

    fix: correct file format in libs/spline

--- libs/spline/rollup.build.cjs
+++ libs/spline/rollup.build.cjs
@@ -1,40 +1,39 @@
-const { terser } = require('rollup-plugin-terser');
+const tsconfig = require('../../tsconfig.base.json');
 const alias = require('@rollup/plugin-alias');
+const path = require('path');
 
 function getRollupOptions(options) {
   const extraGlobals = {};
-  const bundled = ['@splinetool/loader'];
-  const externals = [];
+  const externals = options.output.format === 'cjs' ? ['@digipair-vision/core'] : [];
 
-  return {
+  const value = {
     ...options,
+    external: name => {
+      return externals.includes(name);
+    },
     plugins: [
       ...options.plugins,
       alias({
-        entries: [
+        entries: Object.getOwnPropertyNames(tsconfig.compilerOptions.paths).map(property => ({
+          find: property,
+          replacement: path.resolve(tsconfig.compilerOptions.paths[property][0]),
+        }),
+        [
           { find: 'three/examples/jsm/loaders/DRACOLoader.js', replacement: 'libs/spline/src/lib/vendors/DRACOLoader.js' },
           { find: 'three/examples/jsm/utils/BufferGeometryUtils.js', replacement: 'libs/spline/src/lib/vendors/BufferGeometryUtils.js' },
           { find: 'three', replacement: 'libs/spline/src/lib/vendors/three.js' },
         ]
-      })
+      ),
+      }),
     ],
-    external: (name) => {
-      if (bundled.includes(name)) {
-        return false;
-      }
-
-      if (externals.includes(name)) {
-        return true;
-      }
-
-      return options.external(name);
-    },
     output: {
       ...options.output,
       globals: extraGlobals,
-      plugins: [terser()],
+      plugins: [],
     },
   };
+
+  return value;
 }
 
 module.exports = getRollupOptions;
