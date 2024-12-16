99f0b33601781c0008bb9aa3a628941a83314bde
Raphael Rattelli <raphael.rattelli@epitech.eu>
2024-12-16 11:02:48 +0100

    fix: libs/info is working

--- apps/demo/src/index.ts
+++ apps/demo/src/index.ts
@@ -1,11 +1,11 @@
-// import '@digipair-vision/camrender';
+import '@digipair-vision/camrender';
 import '@digipair-vision/core';
 import '@digipair-vision/design-system';
 // import '@digipair-vision/exporter';
 import '@digipair-vision/html';
-// import '@digipair-vision/info';
+import '@digipair-vision/info';
 import '@digipair-vision/mesh';
-// import '@digipair-vision/observe';
+import '@digipair-vision/observe';
 import '@digipair-vision/player';
 import '@digipair-vision/ready-player-me';
 import '@digipair-vision/router';
--- libs/info/rollup.build.cjs
+++ libs/info/rollup.build.cjs
@@ -4,7 +4,7 @@ const path = require('path');
 
 function getRollupOptions(options) {
   const extraGlobals = {};
-  const externals = options.output.format === 'cjs' ? ['@digipair-vision/core', '@digipair-vision/player', '@digipair-vision/router', '@digipair-vision/html', '@digipair-vision/mesh', '@digipair-vision/teleport', '@digipair-vision/screen-shared', '@digipair-vision/design-system', '@digipair-vision/scene', '@digipair-vision/ready-player-me', '@digipair-vision/camrender', '@digipair-vision/observe', '@digipair-vision/info'] : [];
+  const externals = options.output.format === 'cjs' ? ['@digipair-vision/core', '@digipair-vision/design-system'] : [];
 
   const value = {
     ...options,
